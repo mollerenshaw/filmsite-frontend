b00eb59b60c399e103c4499a942f1fc58df53373
Mark Ollerenshaw <mollerenshaw@zepler.net>
2015-07-18 12:51:54 -0500

    Gruntfile now does lessc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,5 +6,21 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
 
 
+  grunt.initConfig({
+    watch: {
+      files: ['site/css/less/**/*.less'],
+      tasks: ['less']
+    },
 
+    less: {
+      development: {
+        options: {
+          paths: ["bower_components"]
+        },
+        files: {
+          "site/css/styles.css": "site/css/less/styles.less"
+        }
+      }
+    }
+  });
 }
