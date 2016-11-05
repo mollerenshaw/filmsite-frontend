e717ee137cb7ad92ad5ceac90c76a916c800db28
Mark <mollerenshaw@zepler.net>
2016-11-05 12:57:45 +0000

    Tidied up Angular dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,12 @@ module.exports = function (grunt) {
     browserify: {
       dist: {
         files: {
-          'lib/site/js/scripts.js': ['node_modules/angular-material/angular-material.min.js', 'lib/site/js/**/*.js']
+          'lib/site/js/scripts.js': ['node_modules/angular/angular.min.js',
+                                     'node_modules/angular-material/angular-material.min.js', 
+                                     'node_modules/angular-animate/angular-animate.min.js',
+                                     'node_modules/angular-aria/angular-aria.min.js',
+                                     'node_modules/angular-messages/angular-messages.min.js',
+                                     'lib/site/js/**/*.js']
         },
         options: {
         }
